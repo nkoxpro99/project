115dccbb60bc66899aa4ecad005f9181bcdb7349
nkoxpro99 <130723737+nkoxpro99@users.noreply.github.com>
2025-11-11 18:58:24 +0900

    new: change icon for admin dashboard page

--- apps/admin/src/components/customs/CustomLayout.tsx
+++ apps/admin/src/components/customs/CustomLayout.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Layout, Menu } from 'react-admin';
+import { SquareIcon, TimerIcon, StackIcon, RulerSquareIcon, PersonIcon } from '@radix-ui/react-icons';
 
 const MyMenu = () => (
   <Menu>
@@ -8,10 +10,15 @@ const MyMenu = () => (
     <Menu.DashboardItem primaryText="Thống kê"></Menu.DashboardItem>
     <Menu.Item primaryText="Doanh thu" to="/revenue"></Menu.Item>
     <Menu.Item primaryText="Người dùng" to="/users"></Menu.Item>
+    <Menu.DashboardItem primaryText="Kho bãi" leftIcon={<SquareIcon />} />
+    <Menu.Item primaryText="Tất cả" to="/warehouse" leftIcon={<SquareIcon />} />
+    <Menu.Item primaryText="Đang chờ duyệt" to="/request" leftIcon={<TimerIcon />} />
+    <Menu.DashboardItem primaryText="Thống kê" leftIcon={<StackIcon />} />
+    <Menu.Item primaryText="Doanh thu" to="/revenue" leftIcon={<RulerSquareIcon />} />
+    <Menu.Item primaryText="Người dùng" to="/users" leftIcon={<PersonIcon />} />
   </Menu>
 );
 
 export const CustomLayout = (props: any) => {
   return <Layout {...props} menu={MyMenu} />;
 };
-
